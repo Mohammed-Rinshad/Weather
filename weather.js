a031f94650c5c695228cfee89f36ac04cf15922b
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-04-09 22:56:51 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 
-fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=palakkad`)
+fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=palakkad?${zae6LQDSYRn3AQLQ5XVnjGkAhM1BiZ3l}`)
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(error => console.log(error))
