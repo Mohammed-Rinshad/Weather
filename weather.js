50c555278fe812fa20265f21fc4ed0cc4d9a1ceb
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-04-15 16:44:36 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,5 +1,20 @@
+let search = document.getElementById('search')
+let temperature = document.getElementById('temp')
 
-fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=palakkad`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+async function getData(){
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q='+search.value+'&appid=e3d12c319691e524cefe4f135ee68935&units=metric'
+    const res = await fetch(url)
+    const data = await res.json()
+    temperature.textContent = data.main.temp + "°"
+    console.log(data.main.temp)
+
+}
+
+
+//getData()
+
+//.then(res => res.json())
+//.then(data => console.log(data))
+//.catch(error => console.log(error))
+
+
