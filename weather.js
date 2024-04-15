61bc04aad2b04542655a139b20f557c9f069f815
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-04-15 16:54:57 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ let search = document.getElementById('search')
 let temperature = document.getElementById('temp')
 
 async function getData(){
-    const url = 'http://api.openweathermap.org/data/2.5/weather?q='+search.value+'&appid=e3d12c319691e524cefe4f135ee68935&units=metric'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+search.value+'&appid=e3d12c319691e524cefe4f135ee68935&units=metric'
     const res = await fetch(url)
     const data = await res.json()
     temperature.textContent = data.main.temp + "°"
