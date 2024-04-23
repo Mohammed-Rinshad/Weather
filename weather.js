0400a61d63dc5d432943e4173f4d947c0528b198
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-04-23 20:43:59 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 let search = document.getElementById('search')
 let temperature = document.getElementById('temp')
+let feelsLike = document.getElementById('feels-like-results')
 
 async function getData(){
     const url = 'https://api.openweathermap.org/data/2.5/weather?q='+search.value+'&appid=e3d12c319691e524cefe4f135ee68935&units=metric'
@@ -7,8 +8,9 @@ async function getData(){
     const data = await res.json()
     console.log(data)
     if(data.cod === 200){
-        temperature.textContent = data.main.temp + "°"
-        temperature.style.color = "black"
+        temperature.textContent = data.main.temp.toFixed(0) + "°"
+        temperature.style.color = "white"
+        feelsLike.textContent = data.main.feels_like.toFixed(0) + "°"
     } else{
         temperature.textContent = 'City not Found'
         temperature.style.color = "red"
